e747d462d2e4654feb43dba78dfef670a504348a
boseung2 <jungbs3726@gmail.com>
2021-06-13 07:01:03 +0900

    (Backend) Add Google Social Login

--- src/main/java/org/zerock/springreactpractice/security/SecurityConfig.java
+++ src/main/java/org/zerock/springreactpractice/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package org.zerock.springreactpractice.security;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -11,9 +12,11 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.zerock.springreactpractice.security.filter.APILoginFailHandler;
 import org.zerock.springreactpractice.security.filter.ApiCheckFilter;
 import org.zerock.springreactpractice.security.filter.ApiLoginFilter;
+import org.zerock.springreactpractice.security.handler.ClubLoginSuccessHandler;
 
 @Configuration
 @Log4j2
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
@@ -36,10 +39,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return apiLoginFilter;
     }
 
+    @Bean
+    public ClubLoginSuccessHandler successHandler() {
+        return new ClubLoginSuccessHandler();
+    }
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
+        http.formLogin();
+        http.logout();
+
+        http.csrf().disable();
+        http.oauth2Login().successHandler(successHandler());
+        http.rememberMe().tokenValiditySeconds(60*60*24*7).userDetailsService(userDetailsService());
         http.addFilterBefore(apiCheckFilter(), UsernamePasswordAuthenticationFilter.class);
 
     }
--- src/test/java/org/zerock/springreactpractice/repository/ClubMemberTests.java
+++ src/test/java/org/zerock/springreactpractice/repository/ClubMemberTests.java
@@ -23,4 +23,5 @@ public class ClubMemberTests {
         System.out.println(clubMember);
 
     }
+
 }
